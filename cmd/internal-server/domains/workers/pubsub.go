2bb84646ee8af1d4a9f62ad13f2853c927088a29
Eric Masi <eric.masi@sesolabor.com>
2024-05-27 21:09:23 -0400

    Remove comment

--- cmd/internal-server/domains/workers/pubsub.go
+++ cmd/internal-server/domains/workers/pubsub.go
@@ -6,7 +6,6 @@ func NewWorkerPubSub() *events.PubSub[PubSubEvent] {
 	return events.NewPubSub[PubSubEvent]()
 }
 
-// The topic will be something like `CREATED`
 type PubSubEvent struct {
 	WorkerID int64
 }
