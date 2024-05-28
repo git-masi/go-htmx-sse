2d807ed0f45f95040a9b2715ab4ad5c0c17ba1c9
Eric Masi <eric.masi@sesolabor.com>
2024-05-28 07:46:37 -0400

    Use plural table name for consistency

--- migrations/1716737240_add_pay_periods_table.up.sql
+++ migrations/1716737240_add_pay_periods_table.up.sql
@@ -1,5 +1,5 @@
 -- Migration for: add_pay_period_table (UP)
-CREATE TABLE pay_period (
+CREATE TABLE pay_periods (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     start_date TEXT NOT NULL,
     end_date TEXT NOT NULL,
