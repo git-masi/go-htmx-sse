0adc6239aa185239a585b014a5d2dd6a0a00838f
Eric Masi <eric.masi@sesolabor.com>
2024-05-27 21:07:12 -0400

    Add status to workers

--- migrations/1716736179_add_workers_table.up.sql
+++ migrations/1716736179_add_workers_table.up.sql
@@ -3,5 +3,6 @@ CREATE TABLE workers (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
-    email TEXT UNIQUE NOT NULL
+    email TEXT UNIQUE NOT NULL,
+    status TEXT NOT NULL
 );
